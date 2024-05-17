eaf8f411940743cba27f3e3adbdc17163f1622b1
NickiSamantha <nickisabels73@gmail.com>
2024-05-17 10:40:07 +0200

    all done

--- script/code.js
+++ script/code.js
@@ -1,19 +1,29 @@
-function convertTemperature() {
-    const inputValue = document.querySelector('.inputValue').value;
-    const inputUnit = document.querySelector('.inputUnit').value;
-    let result;
+document.addEventListener('DOMContentLoaded', () => {
+    const convertButton = document.querySelector('.convertButton');
+    function convertTemperature() {
+        const inputValue = document.querySelector('.inputValue').value;
+        const inputUnit = document.querySelector('.inputUnit').value;
+        let result;
 
-    if (isNaN(inputValue) || inputValue === '') {
-        result = 'Please enter a valid number';
-    } else {
-        const value = parseFloat(inputValue);
-
-        if (inputUnit === 'Celsius') {
-            result = `${value} °C is ${(value * 9/5 + 32).toFixed(2)} °F`;
-        } else if (inputUnit === 'Fahrenheit') {
-            result = `${value} °F is ${((value - 32) * 5/9).toFixed(2)} °C`;
-        }
-    }
+        if (isNaN(inputValue) || inputValue === '') {
+            result = 'Please enter a valid number';
+        } else {
+            const value = parseFloat(inputValue);
     
-    document.querySelector('.result').innerText = result;
-}
+            if (inputUnit === 'Celsius') {
+                result = `${value} °C is ${(value * 9/5 + 32).toFixed(2)} °F`;
+            } else if (inputUnit === 'Fahrenheit') {
+                result = `${value} °F is ${((value - 32) * 5/9).toFixed(2)} °C`;
+            }
+        }
+    
+        document.querySelector('.result').innerText = result;
+    }
+
+
+});
+
+
+
+
+    
